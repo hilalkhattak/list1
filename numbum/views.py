89fabe40c63ebef66ea255ee47da8d489466dc00
hilal <hilalkhattak@yahoo.com>
2020-11-26 18:29:27 +0000

    commit

---
+++ numbum/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("<h1>hello there</h1>")
+
+
+def rooms(request):
+    return HttpResponse("<h1>Rooms</h1>")
